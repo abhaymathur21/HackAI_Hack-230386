2e817789454fea2e7750c412eed00b44d4713949
Abhay Mathur <a21.mathur21@gmail.com>
2023-10-05 14:14:12 +0530

    coloured the terminal alert

--- src/agents/temperature/message_printer.py
+++ src/agents/temperature/message_printer.py
@@ -1,6 +1,9 @@
 from uagents import Agent, Context, Bureau, Model
 from uagents.setup import fund_agent_if_low
 from messages.messages import Alert
+from colored import Fore, Back, Style
+
+color: str = f'{Style.underline}{Fore.cyan}{Back.black}'
 
 message_printer = Agent(name="message_printer", endpoint =["http://localhost:8001"], port=8001)
 
@@ -8,4 +11,4 @@ fund_agent_if_low(message_printer.wallet.address())
 
 @message_printer.on_message(model = Alert)
 async def message_handler(ctx: Context,sender:str,message: Alert):
-    ctx.logger.info(f"Received msg from {ctx.name}: {message.message}")
+    ctx.logger.info(f"{color}Received msg from {ctx.name}: {message.message}{Style.reset}")
